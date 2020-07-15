98ce61a42ebaaaed317f01c201e5c4d899841616
error0 <599727173@qq.com>
2020-07-15 10:16:41 +0800

    First Build Spring

---
+++ cn.error0.Spring/src/main/java/UserTest.java
@@ -0,0 +1,11 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class UserTest {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("User.xml");
+		User user= (User) context.getBean("user");
+		System.out.println(user);
+	}
+}
